cffa89803c6e2e9ae7748504e76c55b21d1709e5
kirito1412221 <2052753456@qq.com>
2022-08-11 02:06:03 +0800

    test

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -44,11 +44,13 @@ const actions = {
     // }
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
+        console.log('good')
         const { data } = response
         commit('SET_TOKEN', data.token)
         setToken(data.token)
         resolve()
       }).catch(error => {
+        console.log('err')
         reject(error)
       })
     })
